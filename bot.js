b83346a17ff56bca212f652c895044017bd30ca7
mohsky123 <42079362+mohsky123@users.noreply.github.com>
2018-08-05 03:59:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,31 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const ms = require("ms");
-var jimp = require('jimp')
-const db = require('quick.db')
-const arraySort = require('array-sort'),
-      table = require('table');
-const translate = require('google-translate-api');
-const moment = require('moment');
-const ytdl = require('ytdl-core');
-const Codes = require('codes-official');
-const request = require('request');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const math = require('math-expression-evaluator');
-const stripIndents = require('common-tags').stripIndents;
-const pretty = require('pretty-ms');
-const rn = require('random-number');
-let done = {};
-const child_process = require("child_process");
-const adminprefix = "-";
-const devs = ['400943733790605315'];
-const shorten = require('isgd');
-var prefix = '-';
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
 client.login(process.env.BOT_TOKEN);
